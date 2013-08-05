d662f08ea6d758f54ccc8790cd0a0b2c1da8f4ff
navarhs <shravan.v.jadhav@gmail.com>
2013-08-05 10:40:22 +0100

    Added basic keypad

--- app.js
+++ app.js
@@ -42,14 +42,5 @@ app.get('/', function (req, res) {
  * Sockets
  */
 io.sockets.on('connection', function (socket) {
-
-  socket.emit('news', { hello: 'world' })
-
-  socket.on('my other event', function (data) {
-    console.log(data)
-  })
-
-  socket.emit('five')
-  socket.emit('seven')
-
+  
 })
