d68eef625d686f281cd3c3f9cdd5c7680ce4f982
navarhs <shravan.v.jadhav@gmail.com>
2013-08-07 11:15:42 +0100

    Removed svg-based keypad and tweaked css keypad

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
-  //drawKeypad('keypad');
-  drawDivKeypad('keys');
+  drawKeypad('keys');
 
   var socket = io.connect('http://localhost');
   socket.on('call', function (data) {
@@ -9,63 +8,12 @@ $(document).ready(function () {
 
   socket.on('tone', function (data) {
     tone = data.tone;
-    // $('#key' + tone).attr('fill', 'red');
-    // setTimeout("$('#key" + tone + "').attr('fill', 'green');", 300);
     $('#divKey' + tone).toggleClass('checked');
-    setTimeout("$('#divKey" + tone + "').toggleClass('checked');", 300)
+    setTimeout("$('#key" + tone + "').toggleClass('checked');", 300)
   });
 });
 
-function drawKeypad(svg) {
-  var svg = document.getElementById(svg);
-  var svgns = 'http://www.w3.org/2000/svg';
-
-  var k = 1;
-  for (var i=1; i<=4; i++) {
-    for (var j=1; j<=3; j++) {
-      var val = k;
-      switch (k)
-      {
-        case 10: val = 'star'; break;
-        case 11: val = 0; break;
-        case 12: val = 'hash'; break;
-      }
-      var y = svg.getAttribute('height') * 1/5 * i;
-      var x = svg.getAttribute('width') * 1/4 * j;
-
-      var circle = document.createElementNS(svgns, 'circle');
-      circle.setAttribute('id', 'key' + val);
-      circle.setAttribute('cx', x);
-      circle.setAttribute('cy', y);
-      circle.setAttribute('r', '20');
-      circle.setAttribute('fill', 'green');
-      
-      var text = document.createElementNS(svgns, 'text');
-      text.setAttribute('x', x);
-      text.setAttribute('y', y);
-      text.setAttribute('fill', 'white');
-      text.setAttribute('style', 'text-anchor: middle; alignment-baseline: middle');
-      text.setAttribute('font-family', "'Arial'");
-      text.setAttribute('font-size', '16');
-      text.setAttribute('font-weight', 'bold');
-      if (val == 'star')
-        text.textContent = '\u2731';
-      else {
-        if (val == 'hash')
-          text.textContent = '#';
-        else
-          text.textContent = val;
-      }
-
-      svg.appendChild(circle);
-      svg.appendChild(text);
-
-      k++;
-    }
-  }
-}
-
-function drawDivKeypad(div) {
+function drawKeypad(div) {
   var div = document.getElementById(div);
 
   var k = 1;
@@ -84,7 +32,7 @@ function drawDivKeypad(div) {
 
       var key = document.createElement('div');
       key.className = 'key';
-      key.setAttribute('id', 'divKey' + val);
+      key.setAttribute('id', 'key' + val);
 
       var keyContent = document.createElement('b');
       keyContent.className = 'keyContent';
