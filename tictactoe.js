8249e7f4570421ff22ca8ab88adf86818c2e5729
navarhs <shravan.v.jadhav@gmail.com>
2013-08-07 15:53:16 +0100

    Removed commmented method

--- tictactoe.js
+++ tictactoe.js
@@ -26,36 +26,6 @@ exports.addPlayer = function (game, player, sockets) {
 	}
 }
 
-// exports.isWon = function (b) {
-// 	// horizontals
-// 	if ((b[0] == b[1] && b[1] == b[2]) && b[0])
-// 		return b[0];
-
-// 	if ((b[3] == b[4] && b[4] == b[5]) && b[3])
-// 		return b[3];
-
-// 	if ((b[6] == b[7] && b[7] == b[8]) && b[6])
-// 		return b[6];
-
-// 	// verticals
-// 	if ((b[0] == b[3] && b[3] == b[6]) && b[0])
-// 		return b[0];
-
-// 	if ((b[1] == b[4] && b[4] == b[7]) && b[1])
-// 		return b[1];
-
-// 	if ((b[2] == b[5] && b[5] == b[8]) && b[2])
-// 		return b[2];
-
-// 	// diagonals
-// 	if ((b[0] == b[4] && b[4] == b[8]) && b[0])
-// 		return b[0];
-
-// 	if ((b[6] == b[4] && b[4] == b[2]) && b[6])
-// 		return b[6];
-// 	return 0;
-// }
-
 exports.isWon = function (b) {
 	Result = function () {
 		this.winner = 0;
