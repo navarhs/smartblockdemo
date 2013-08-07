7ae3a88439cf65d7b72ccebdfd3406f349a321e3
navarhs <shravan.v.jadhav@gmail.com>
2013-08-07 11:57:45 +0100

    Formatted site

--- public/js/tictactoe.js
+++ public/js/tictactoe.js
@@ -2,11 +2,11 @@ $(document).ready(function () {
   var socket = io.connect('http://localhost');
 
   socket.on('player1', function (data) {
-    $('#player1').html("Player 1 connected: <br/>" + data.player);
+    $('#player1').html("player connected: <br/>" + data.player);
   });
 
   socket.on('player2', function (data) {
-    $('#player2').html("Player 2 connected: <br/>" + data.player);
+    $('#player2').html("player connected: <br/>" + data.player);
   });
 
   socket.on('move', function (data) {
