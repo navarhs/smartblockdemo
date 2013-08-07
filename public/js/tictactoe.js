1a14e4d15bfb3076c5d5cea2de7b5e2e52e42173
navarhs <shravan.v.jadhav@gmail.com>
2013-08-07 14:35:02 +0100

    Minor formatting

--- public/js/tictactoe.js
+++ public/js/tictactoe.js
@@ -2,21 +2,21 @@ $(document).ready(function () {
   var socket = io.connect('http://localhost');
 
   socket.on('player1', function (data) {
-    $('#player1').html("player connected: <br/>" + data.player);
-    $('#log').html(data.player + " has connected as player 1. Awaiting opponent.")
+    $('#player1').html("Player connected: <br/>" + data.player);
+    $('#log').html(data.player + " has connected as player 1. <br/>Awaiting an opponent.")
   });
 
   socket.on('player2', function (data) {
-    $('#player2').html("player connected: <br/>" + data.player);
-    $('#log').html(data.player + " has connected as player 2. Game starting.")
+    $('#player2').html("Player connected: <br/>" + data.player);
+    $('#log').html(data.player + " has connected as player 2. <br/>Game starting. Player 1's turn.")
   });
 
   socket.on('move', function (data) {
     $('#b' + data.square).text(data.sign);
     if (data.sign == 'X')
-      $('#log').html("Player 1 has moved. Player 2's turn.");
+      $('#log').html("Player 1 has moved. <br/>Player 2's turn.");
     else
-      $('#log').html("Player 2 has moved. Player 1's turn.");
+      $('#log').html("Player 2 has moved. <br/>Player 1's turn.");
   });
 
   socket.on('log', function (data) {
@@ -25,12 +25,12 @@ $(document).ready(function () {
 
   socket.on('gameOver', function (data) {
     if (data.winner == 1)
-      $('#log').html("Game over. The winner is player 1.")
+      $('#log').html("Game over. <br/>The winner is player 1!")
     else {
       if (data.winner == 2)
-        $('#log').html("Game over. The winner is player 2.")
+        $('#log').html("Game over. <br/>The winner is player 2!")
       else
-        $('#log').html("Game over. The game is a draw.")
+        $('#log').html("Game over. <br/>The game is a draw!")
     }
   });
 });
